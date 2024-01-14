598e2be6af23505bb91e8ded13d76fba23ac1bc5
Roman <roman@lerchster.dev>
2024-01-14 22:31:56 +0100

    add simple remove shorts script

--- src/contentScript/index.ts
+++ src/contentScript/index.ts
@@ -1,1 +1,19 @@
-console.log(123);
+import logger from "./logger";
+
+let intervalId: NodeJS.Timeout;
+
+window.addEventListener("load", () => {
+  clearInterval(intervalId);
+
+  intervalId = setInterval(() => {
+    const navigationMenuItems = document.querySelector(
+      "#sections > *:first-child > #items",
+    )?.children;
+
+    if (!navigationMenuItems || navigationMenuItems.length < 3) return;
+
+    navigationMenuItems.item(1)?.remove();
+
+    logger.log("Removed Shorts tab");
+  }, 10);
+});
